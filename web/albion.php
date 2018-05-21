b16f9eed380851f0a85762d59488bf322bfd758a
Benoit Lemaitre <lemaitre.b.perso@gmail.com>
2018-05-21 21:52:31 +0200

    Add getResourcePrices

--- web/albion.php
+++ web/albion.php
@@ -1,4 +1,40 @@
 <?php 
 
-$html = file_get_contents('https://www.albion-online-data.com/api/v1/stats/prices/T5_ORE');
-var_dump(json_decode($html));
+define('RARITY_STRING', "_ORE_LEVEL1@");
+define('MARKET', "Caerleon Market");
+define('API_URL', "https://www.albion-online-data.com/api/v1/stats/prices/");
+
+$tiers = ["4","5","6","7","8"];
+$resourcesTypes = ["PLANKS", "METALBAR", "LEATHER", "CLOTH"];
+
+$resourcePrices = getResourcePrices($tiers);
+
+// Ressources : ["PLANKS", "METALBAR", "LEATHER", "CLOTH"]
+$recipes = [
+	[
+		"Warbow",
+		"2H_WARBOW",
+		[,,,]
+	]
+]
+
+function getResourcePrices($resourcesTypes, $tiers, rarity = "") {
+
+	$resourcePrices = [];
+
+	foreach ($resourcesTypes as $resourceType) {
+		foreach ($tiers as $tier) {
+			$requestResult = file_get_contents(API_URL."T".$tier."_".$resourceType);
+			$jsonResults = json_decode($requestResult));
+
+			foreach ($jsonResults as $result) {
+				if ($result['city'] === MARKET) {
+					$resourcePrices[$resourceType][$tier] = $result['sell_price_min'];
+				}
+			}
+		}
+	}
+
+}
+
+var_dump($resourcePrices);
