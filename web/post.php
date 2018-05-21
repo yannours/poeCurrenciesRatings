c882eeb6d319ad943cd30f4aee24e117a920fffc
Benoit Lemaitre <lemaitre.b.perso@gmail.com>
2018-05-21 16:12:14 +0200

    Add request debug

--- web/post.php
+++ web/post.php
@@ -1,10 +1,5 @@
 <?php 
 $content = "test post php".PHP_EOL;
-foreach ($_POST as $key => $value) {
- 	$content .= "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value).PHP_EOL;
-}
-foreach ($_GET as $key => $value) {
- 	$content .= "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value).PHP_EOL;
-}
+$content .= print_r($_REQUEST);
 
 file_put_contents("log.txt", $content, FILE_APPEND);
