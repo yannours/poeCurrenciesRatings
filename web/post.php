89966153ad80b936ce70bd1d0864808cc2fb4682
Benoit Lemaitre <lemaitre.b.perso@gmail.com>
2018-05-21 16:02:18 +0200

    Add get

--- web/post.php
+++ web/post.php
@@ -1,6 +1,10 @@
 <?php 
 $content = "test post php".PHP_EOL;
-foreach ($_POST as $key => $value)
- $content .= "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value).PHP_EOL;
+foreach ($_POST as $key => $value) {
+ 	$content .= "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value).PHP_EOL;
+}
+foreach ($_GET as $key => $value) {
+ 	$content .= "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value).PHP_EOL;
+}
 
 file_put_contents("log.txt", $content, FILE_APPEND);
