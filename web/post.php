5630673fcdc467686f21482281bf850f81cfeb05
Benoit Lemaitre <lemaitre.b.perso@gmail.com>
2018-05-21 16:13:13 +0200

    Add print_r to screen

--- web/post.php
+++ web/post.php
@@ -1,5 +1,7 @@
 <?php 
 $content = "test post php".PHP_EOL;
-$content .= print_r($_REQUEST);
+$content .= print_r($_REQUEST, true);
 
 file_put_contents("log.txt", $content, FILE_APPEND);
+
+print_r(file_get_contents("log.txt"));
