292fccde2ec749b89d9b34307ad857200f96d166
Benoit Lemaitre <lemaitre.b.perso@gmail.com>
2018-05-23 19:06:59 +0200

    Change date time

--- config.php
+++ config.php
@@ -9,8 +9,8 @@ define('API_URL', "https://www.albion-online-data.com/api/v1/stats/prices/");
 define('MARKET', "Caerleon Market");
 define('RARITY_STRING', "_LEVEL1@");
 
-// Price must be newer than this date
-define('MIN_PRICE_DATE', date(DATE_ATOM, mktime(date("H")-1)));
+// Price must be newer than this date. H-2 = server time.
+define('MIN_PRICE_DATE', date(DATE_ATOM, mktime(date("H")-3)));
 
 $resourcesTypes = [
 	"WOOD" => "PLANKS",
