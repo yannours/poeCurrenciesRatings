0d187d1a5a36c516d34bbbefc7ecdabeaf8690e9
Benoit Lemaitre <lemaitre.b.perso@gmail.com>
2018-05-27 19:26:33 +0200

    Fix enchanted refining price

--- web/refining.php
+++ web/refining.php
@@ -8,9 +8,9 @@ require_once("../config.php");
 require_once("../resources/pricesHistory.php");
 require_once("../resources/pricesCalculation.php");
 
-$resourcesPrices = getLatestPrices(array_merge(array_keys($resourcesTypes), $resourcesTypes), [3, 4, 5, 6]);
-$refiningCosts = getResourcesRefiningCost($resourcesTypes, [4, 5, 6], $resourcesPrices);
-$refiningProfits = getResourcesRefiningProfit($resourcesTypes, [4, 5, 6], $resourcesPrices, $refiningCosts);
+$resourcesPrices = getLatestPrices(array_merge(array_keys($resourcesTypes), $resourcesTypes), [3, 4, 5, 6, 7, 8], 0);
+$refiningCosts = getResourcesRefiningCost($resourcesTypes, [4, 5, 6, 7, 8], $resourcesPrices);
+$refiningProfits = getResourcesRefiningProfit($resourcesTypes, [4, 5, 6, 7, 8], $resourcesPrices, $refiningCosts);
 
 print_r($refiningProfits);
 
