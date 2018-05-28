0d1007003de93e8ec5c23b33700c4f84e92b260e
Benoit <benoit@youboostmarket.fr>
2018-05-28 14:01:35 +0200

    Doc rework

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 Work in progress
 
-Todo :
-	Vérifier ce que ça donne avec des T1
-	Faire une vue pour avoir les prix
-	supprimer api.php
+Works with informations from Albion Data Client.
+In order to redirect the ADC to this page, launch it with -i="websiteadresse/albionDataClient.php"
+
+More on ADC : https://github.com/broderickhyman/albiondata-client
+Site inspired by http://albionassistant.com/calculator.aspx
+
+Note on location codes :
+$locationsCode = [
+	-1 => "Unknown",
+	0 => "ThetfordMarket",
+	1000 => "LymhurstMarket",
+	2000 => "BridgewatchMarket",
+	3003 => "BlackMarket",
+	3004 => "MartlockMarket",
+	3005 => "CaerleonMarket",
+	4000 => "FortSterlingMarket",
+
+	4 => "SwampCrossMarket",
+	1006 => "ForestCrossMarket",
+	2002 => "SteppeCrossMarket",
+	3002 => "HighlandCrossMarket",
+	4006 => "MountainCrossMarket",
+];
--- web/albionDataClient.php
+++ web/albionDataClient.php
@@ -3,32 +3,12 @@
  * Landing page of the informations sent by the albion data client.
  * Save the datas to a database
  * In order to redirect the ADC to this page, launch it with -i="websiteadresse/albionDataClient.php"
- * More on ADC : https://github.com/broderickhyman/albiondata-client
- * Site inspired by http://albionassistant.com/calculator.aspx
  */
 
 require_once("../resources/pricesHistory.php");
 
 $post = file_get_contents('php://input');
-//$post = file_get_contents('../resources/ADCReturnExample.json');
 $json = json_decode($post, true);
 
 // Decode and save orders
 savePricesToDB($json['Orders']);
-
-$locationsCode = [
-	-1 => "Unknown",
-	0 => "ThetfordMarket",
-	1000 => "LymhurstMarket",
-	2000 => "BridgewatchMarket",
-	3003 => "BlackMarket",
-	3004 => "MartlockMarket",
-	3005 => "CaerleonMarket",
-	4000 => "FortSterlingMarket",
-
-	4 => "SwampCrossMarket",
-	1006 => "ForestCrossMarket",
-	2002 => "SteppeCrossMarket",
-	3002 => "HighlandCrossMarket",
-	4006 => "MountainCrossMarket",
-];
