b8afc685ec51d6b44f5676b9a24dc16267be874e
Benoit Lemaitre <lemaitre.b.perso@gmail.com>
2018-06-07 08:51:20 +0200

    Add try catch

--- back/adcNats/marketOrders.php
+++ back/adcNats/marketOrders.php
@@ -31,43 +31,47 @@ $connectionOptions->setHost('192.241.250.27')->setPort(4222)->setUser('public')-
 // The connection is not supposed to be stop, but in this event, we relaunch it as ofter as necessary
 while (1) {
 
-	// New connection
-	$client = new \Nats\Connection($connectionOptions);
-	$client->setStreamTimeout(9000000);
-	$client->connect();
+	try {
+		// New connection
+		$client = new \Nats\Connection($connectionOptions);
+		$client->setStreamTimeout(9000000);
+		$client->connect();
 
-	$client->subscribe(
-	    'marketorders.ingest',
-	    function ($results) {
-	    	$results = json_decode($results->getBody(), true);
+		$client->subscribe(
+		    'marketorders.ingest',
+		    function ($results) {
+		    	$results = json_decode($results->getBody(), true);
 
-	    	if(!empty($results['Orders'])) {
-	    		$ordersList = $results['Orders'];
+		    	if(!empty($results['Orders'])) {
+		    		$ordersList = $results['Orders'];
 
-		        $prices = [];
-			    $dbConnection = new PDO("mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_BASE, DB_USER, DB_PASSWORD);
-			    $insertStatement = $dbConnection->prepare("INSERT INTO item_prices_history (item_id, location, price) VALUES(?, ?, ?)");
+			        $prices = [];
+				    $dbConnection = new PDO("mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_BASE, DB_USER, DB_PASSWORD);
+				    $insertStatement = $dbConnection->prepare("INSERT INTO item_prices_history (item_id, location, price) VALUES(?, ?, ?)");
 
-				// All order from the lists are processed in order to get, for each item, the cheaper one
-			    foreach ($ordersList as $order) {
-					if ($order['AuctionType'] == 'offer') {
-				        if (empty($prices[$order['ItemTypeId']]) or $order['UnitPriceSilver'] < $prices[$order['ItemTypeId']]['price']) {
+					// All order from the lists are processed in order to get, for each item, the cheaper one
+				    foreach ($ordersList as $order) {
+						if ($order['AuctionType'] == 'offer') {
+					        if (empty($prices[$order['ItemTypeId']]) or $order['UnitPriceSilver'] < $prices[$order['ItemTypeId']]['price']) {
 
-				            $prices[$order['ItemTypeId']] = [
-				                'location' => $order['LocationId'],
-				                'price' => $order['UnitPriceSilver']
-				            ];
-				        }
+					            $prices[$order['ItemTypeId']] = [
+					                'location' => $order['LocationId'],
+					                'price' => $order['UnitPriceSilver']
+					            ];
+					        }
+					    }
+			    	}
+
+					// Once all prices are fixed, we update them into the database
+				    foreach ($prices as $item => $priceDatas) {
+				        $insertStatement->execute([$item, $priceDatas['location'], ($priceDatas['price']/10000)]);
 				    }
-		    	}
-
-				// Once all prices are fixed, we update them into the database
-			    foreach ($prices as $item => $priceDatas) {
-			        $insertStatement->execute([$item, $priceDatas['location'], ($priceDatas['price']/10000)]);
 			    }
 		    }
-	    }
-	);
+		);
 
-	$client->wait();
+		$client->wait();	
+	} catch (Exception $e) {
+		print_r($e->getMessage().PHP_EOL);
+	}
 }
